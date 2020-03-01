58c9fc6ee2017b8ed70b3f5017928067f12b667b
Oliver <oliverwhk@gmail.com>
2020-03-01 13:36:58 +1300

    Refactored process command.

--- ToyRobotSimulator/RobotSimulator.cs
+++ ToyRobotSimulator/RobotSimulator.cs
@@ -21,24 +21,26 @@ namespace ToyRobotSimulator
                 var y = int.Parse(commaSplits[1]);
                 var direction = (Direction) Enum.Parse(typeof(Direction), commaSplits[2], true);
                 _toyRobot.Place(new Position(x, y), direction);
+                
+                return null;
             }
 
-            var isRobotOnTable = _toyRobot.IsOnTable;
+            if (!_toyRobot.IsOnTable) return null;
 
-            if (command == "REPORT" && isRobotOnTable)
+            if (command == "REPORT")
             {
                 return $"{_toyRobot.Position.X},{_toyRobot.Position.Y},{_toyRobot.Direction.ToString().ToUpper()}";
             }
 
-            if (command == "MOVE" && isRobotOnTable)
+            if (command == "MOVE")
             {
                 _toyRobot.MoveForward();
             }
-            else if (command == "LEFT" && isRobotOnTable)
+            else if (command == "LEFT")
             {
                 _toyRobot.TurnLeft();
             }
-            else if (command == "RIGHT" && isRobotOnTable)
+            else if (command == "RIGHT")
             {
                 _toyRobot.TurnRight();
             }
